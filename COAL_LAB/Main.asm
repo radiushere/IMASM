e4133ec357e7d5a0c22549af5f6bd4f6dde075d6
radiushere <aqibhussain2007@gmail.com>
2026-05-09 19:24:49 +0500

    Phase 6: Save Image and Grayscale Filter

--- COAL_LAB/Main.asm
+++ COAL_LAB/Main.asm
@@ -20,12 +20,17 @@ MF_POPUP            EQU 00000010h
 IDM_FILE_OPEN       EQU 1001    
 IDM_FILE_SAVE       EQU 1002    
 IDM_FILE_EXIT       EQU 1003    
-IDM_FILTER_NEG      EQU 2001    ; NEW: Filter ID
+IDM_FILTER_NEG      EQU 2001    
+IDM_FILTER_GRAY     EQU 2002    ; NEW: Grayscale Filter ID
 
 OFN_FILEMUSTEXIST   EQU 00001000h
 OFN_PATHMUSTEXIST   EQU 00000800h
+OFN_OVERWRITEPROMPT EQU 00000002h ; NEW: Warns if overwriting a file
+
 GENERIC_READ        EQU 80000000h
+GENERIC_WRITE       EQU 40000000h ; NEW: Permission to write to disk
 OPEN_EXISTING       EQU 3
+CREATE_ALWAYS       EQU 2         ; NEW: Creates a new file
 FILE_ATTRIBUTE_NORMAL EQU 80h
 INVALID_HANDLE_VALUE  EQU -1
 
@@ -95,12 +100,11 @@ CreateMenu       PROTO
 CreatePopupMenu  PROTO
 AppendMenuA      PROTO :DWORD, :DWORD, :DWORD, :DWORD
 SetMenu          PROTO :DWORD, :DWORD
-GetOpenFileNameA PROTO :DWORD
 
-; --- FIX: Added GetFileSize prototype! ---
+GetOpenFileNameA PROTO :DWORD
+GetSaveFileNameA PROTO :DWORD       ; NEW: Save File Dialog Prototype
 GetFileSize      PROTO :DWORD, :DWORD
 
-; Graphics API (GDI32) for painting the image
 SetDIBitsToDevice PROTO :DWORD, :DWORD, :DWORD, :DWORD, :DWORD, :DWORD, :DWORD, :DWORD, :DWORD, :DWORD, :DWORD, :DWORD
 GetDC            PROTO :DWORD
 ReleaseDC        PROTO :DWORD, :DWORD
@@ -119,19 +123,20 @@ guiMsg      GUI_MSG <>
 wc          GUI_WNDCLASS <>
 globalInst  DWORD ?          
 
-; Menus
 fileMenuStr BYTE "File",0
 filtMenuStr BYTE "Filters",0
 openStr     BYTE "Open Image...",0
-saveStr     BYTE "Save Image...",0
+saveStr     BYTE "Save Image As...",0
 exitStr     BYTE "Exit",0
 negStr      BYTE "Apply Negative / Invert",0
+grayStr     BYTE "Apply Grayscale (B&W)",0   ; NEW: Menu Text
 
 ofn         GUI_OPENFILENAME <>
 szFileName  BYTE 260 DUP(0)      
 szFilter    BYTE "Bitmap Files (*.bmp)",0,"*.bmp",0,"All Files (*.*)",0,"*.*",0,0
+saveSuccess BYTE "Image successfully saved to disk!",0
+msgTitle    BYTE "IMASM Event",0
 
-; MEGA ENGINE VARIABLES 
 hFile       DWORD ?                 
 bytesRead   DWORD ?                 
 fileSize    DWORD ?                 
@@ -184,7 +189,6 @@ AppWinMain PROC instHandle:DWORD, prevInst:DWORD, cmdLineStr:DWORD, showCmd:DWOR
         CW_USEDEFAULT, CW_USEDEFAULT, 1024, 768, 0, 0, instHandle, 0
     mov mainHwnd, eax
 
-    ; BUILD THE MENUS 
     INVOKE CreateMenu                       
     mov hMenu, eax
     INVOKE CreatePopupMenu                  
@@ -195,7 +199,9 @@ AppWinMain PROC instHandle:DWORD, prevInst:DWORD, cmdLineStr:DWORD, showCmd:DWOR
     INVOKE AppendMenuA, hFileMenu, MF_STRING, IDM_FILE_OPEN, ADDR openStr
     INVOKE AppendMenuA, hFileMenu, MF_STRING, IDM_FILE_SAVE, ADDR saveStr
     INVOKE AppendMenuA, hFileMenu, MF_STRING, IDM_FILE_EXIT, ADDR exitStr
+    
     INVOKE AppendMenuA, hFiltMenu, MF_STRING, IDM_FILTER_NEG, ADDR negStr
+    INVOKE AppendMenuA, hFiltMenu, MF_STRING, IDM_FILTER_GRAY, ADDR grayStr
     
     INVOKE AppendMenuA, hMenu, MF_POPUP, hFileMenu, ADDR fileMenuStr
     INVOKE AppendMenuA, hMenu, MF_POPUP, hFiltMenu, ADDR filtMenuStr
@@ -235,72 +241,133 @@ AppWndProc PROC winHandle:DWORD, msgID:DWORD, wPrm:DWORD, lPrm:DWORD
             
             INVOKE GetOpenFileNameA, ADDR ofn
             .IF eax != 0
-                ; 1. OPEN FILE
                 INVOKE CreateFileA, ADDR szFileName, GENERIC_READ, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0
                 mov hFile, eax
                 
                 .IF eax != INVALID_HANDLE_VALUE
-                    ; 2. GET EXACT FILE SIZE
                     INVOKE GetFileSize, hFile, 0
                     mov fileSize, eax
 
-                    ; 3. ALLOCATE HEAP MEMORY FOR THE WHOLE FILE
                     INVOKE GetProcessHeap
                     mov hHeap, eax
                     INVOKE HeapAlloc, hHeap, 8, fileSize
                     mov pHeap, eax
 
-                    ; 4. READ EVERYTHING INTO RAM
                     INVOKE ReadFile, hFile, pHeap, fileSize, ADDR bytesRead, 0
                     INVOKE CloseHandle, hFile
 
-                    ; 5. SETUP POINTERS (The Magic Math)
                     mov esi, pHeap
                     mov eax, DWORD PTR [esi + 18]
                     mov imgWidth, eax
                     mov eax, DWORD PTR [esi + 22]
                     mov imgHeight, eax
-                    
                     mov eax, DWORD PTR [esi + 10]
                     mov pixelOffset, eax
                     
-                    ; Set pPixels exactly to where the colors start
                     mov eax, pHeap
                     add eax, pixelOffset
                     mov pPixels, eax
                     
-                    ; Set pHeader to the start of the BitmapInfoHeader
                     mov eax, pHeap
                     add eax, 14
                     mov pHeader, eax
 
-                    ; 6. DRAW IT!
                     INVOKE RenderImage, winHandle
                 .ENDIF
             .ENDIF
             
         ; =====================================================================
-        ; THE NEGATIVE FILTER (Raw Assembly Power)
+        ; NEW: SAVE FILE TO DISK
         ; =====================================================================
+        .ELSEIF eax == IDM_FILE_SAVE
+            .IF pHeap != 0
+                mov szFileName[0], 0
+                mov ofn.lStructSize, 76     
+                mov eax, winHandle
+                mov ofn.hwndOwner, eax
+                mov ofn.lpstrFilter, OFFSET szFilter
+                mov ofn.lpstrFile, OFFSET szFileName
+                mov ofn.nMaxFile, 260
+                mov ofn.Flags, OFN_OVERWRITEPROMPT
+                
+                ; Pop up the "Save As" Dialog
+                INVOKE GetSaveFileNameA, ADDR ofn
+                .IF eax != 0
+                    ; Create a BRAND NEW file with Write permissions
+                    INVOKE CreateFileA, ADDR szFileName, GENERIC_WRITE, 0, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0
+                    mov hFile, eax
+                    
+                    .IF eax != INVALID_HANDLE_VALUE
+                        ; Dump our memory buffer (pHeap) straight into the new file
+                        INVOKE WriteFile, hFile, pHeap, fileSize, ADDR bytesRead, 0
+                        INVOKE CloseHandle, hFile
+                        
+                        ; Alert the user of success!
+                        INVOKE MessageBoxA, winHandle, ADDR saveSuccess, ADDR msgTitle, 0
+                    .ENDIF
+                .ENDIF
+            .ENDIF
+            
         .ELSEIF eax == IDM_FILTER_NEG
             .IF pHeap != 0
+                mov ecx, fileSize
+                sub ecx, pixelOffset
+                mov esi, pPixels
                 
-                ; Calculate exactly how many color bytes we have
+            InvertLoop:
+                mov al, BYTE PTR [esi]   
+                not al                   
+                mov BYTE PTR [esi], al   
+                inc esi                  
+                loop InvertLoop          
+                
+                INVOKE RenderImage, winHandle
+            .ENDIF
+
+        ; =====================================================================
+        ; NEW: THE GRAYSCALE (MATH) FILTER
+        ; =====================================================================
+        .ELSEIF eax == IDM_FILTER_GRAY
+            .IF pHeap != 0
+                ; 1. Calculate how many pixels we have
                 mov ecx, fileSize
                 sub ecx, pixelOffset
                 
-                ; Set source pointer to the start of the colors
+                ; Divide byte count by 3 to get pixel count
+                mov eax, ecx
+                mov edx, 0
+                mov ebx, 3
+                div ebx
+                mov ecx, eax    ; ECX now holds total pixels
+
                 mov esi, pPixels
                 
-                ; THE LOOP
-            InvertLoop:
-                mov al, BYTE PTR [esi]   ; Grab 1 byte of color (R, G, or B)
-                not al                   ; Flip its bits! (0 to 1, 1 to 0)
-                mov BYTE PTR [esi], al   ; Put it back in memory
-                inc esi                  ; Move to the next byte
-                loop InvertLoop          ; Repeat until ECX hits 0
+            GrayLoop:
+                ; 2. Load the 3 color channels (Using movzx to prevent overflow in AX)
+                movzx ax, BYTE PTR [esi]      ; Blue
+                movzx bx, BYTE PTR [esi+1]    ; Green
+                movzx dx, BYTE PTR [esi+2]    ; Red
                 
-                ; Redraw the new mutated memory to the screen!
+                ; 3. Add them together (AX = B + G + R)
+                add ax, bx
+                add ax, dx
+                
+                ; 4. Divide by 3 to find the average brightness
+                mov bl, 3
+                div bl          ; AL now contains the average!
+                
+                ; 5. Write the exact same average back to Blue, Green, and Red
+                mov BYTE PTR [esi], al
+                mov BYTE PTR [esi+1], al
+                mov BYTE PTR [esi+2], al
+                
+                ; 6. Jump forward 3 bytes to the next pixel
+                add esi, 3
+                dec ecx
+                cmp ecx, 0
+                jg GrayLoop
+                
+                ; 7. Redraw!
                 INVOKE RenderImage, winHandle
             .ENDIF
         ; =====================================================================
@@ -322,7 +389,6 @@ AppWndProc PROC winHandle:DWORD, msgID:DWORD, wPrm:DWORD, lPrm:DWORD
     ret
 AppWndProc ENDP
 
-; --- CUSTOM PROCEDURE: Paints RAM to the GUI ---
 RenderImage PROC winHandle:DWORD
     LOCAL hdc:DWORD
     
@@ -330,13 +396,8 @@ RenderImage PROC winHandle:DWORD
     mov hdc, eax
     
     INVOKE SetDIBitsToDevice, 
-        hdc, 
-        20, 20,             ; X, Y position on window
-        imgWidth, imgHeight,; Width/Height
-        0, 0, 0, imgHeight, ; Source rectangle
-        pPixels,            ; Pointer to raw colors
-        pHeader,            ; Pointer to header rules
-        0                   ; DIB_RGB_COLORS
+        hdc, 20, 20, imgWidth, imgHeight, 0, 0, 0, imgHeight, 
+        pPixels, pHeader, 0                   
         
     INVOKE ReleaseDC, winHandle, hdc
     ret
